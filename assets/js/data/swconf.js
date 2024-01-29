6f3adb987adeafc027eac9c9ee96cc9bc2a08013
sadefilosofia <133790121+sadefilosofia@users.noreply.github.com>
2024-01-29 21:15:21 +0100

    Add files via upload

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1706558400',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'polyfill.io'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
